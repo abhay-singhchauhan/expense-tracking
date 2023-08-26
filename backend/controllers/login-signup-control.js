f10080d90edc7f6b710b3b548ff50b5bf4f44ad7
abhay singh chauhan <itsyourabhay@gmail.com>
2023-08-26 19:49:00 +0530

    used bcrypt for hashing

--- backend/controllers/login-signup-control.js
+++ backend/controllers/login-signup-control.js
@@ -1,18 +1,27 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res, next) => {
   const parsedData = req.body;
-
   try {
     const emailExists = await User.findAll({
       where: { email: parsedData.email },
     });
-    console.log(emailExists.length == 0);
     if (emailExists.length === 0) {
-      const details = await User.create(parsedData);
-      res.json(details);
+      bcrypt.hash(parsedData.password, 10, async (err, hash) => {
+        if (err) {
+          throw new Error("There is some problem");
+        } else {
+          const details = await User.create({
+            name: parsedData.name,
+            email: parsedData.email,
+            password: hash,
+          });
+          res.json(details);
+        }
+      });
     } else {
-      res.json({ error: true });
+      res.status(500).json({ error: true });
     }
   } catch (err) {
     console.log("there are some problems", err);
@@ -20,26 +29,36 @@ exports.signup = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-  console.log(req.body);
   const userExisted = await User.findAll({
     where: {
       email: req.body.email,
     },
   });
+
   if (userExisted.length === 0) {
     res.status(404).json({
       message: "User dosen't existed, please register yourself",
       problem: "UDE",
     });
-  } else if (userExisted[0].password !== req.body.password.trim()) {
-    res.status(401).json({
-      message: "Please enter the correct password",
-      problem: "UDE",
-    });
   } else {
-    res.status(200).json({
-      message: "Login Successfull",
-      problem: "Success",
-    });
+    bcrypt.compare(
+      req.body.password,
+      userExisted[0].password,
+      (error, success) => {
+        if (success) {
+          console.log(success);
+          res.status(200).json({
+            message: "Login Successfull",
+            problem: "Success",
+          });
+        } else {
+          console.log(success);
+          res.status(401).json({
+            message: "Please enter the correct password",
+            problem: "UDE",
+          });
+        }
+      }
+    );
   }
 };
